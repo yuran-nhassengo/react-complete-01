c96a2b566367e4b4f3ee8bac74fb88b84c762391
yurannhassengo <yurannhassengo@gmail.com>
2024-08-30 01:28:52 -0700

    update UserDeatils

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@
 
 export const App = () => {
 
-    const isAuthenticated = false;
-    
-    return isAuthenticated ? <div>Welcome, User</div> : <div>You are not logged in</div>
+  //NOT_VERIFIED / VERIFIED /ACCOUNT_DISABLED
+
+  const USER_STATUS = "VERIFIED";
+
+  if(USER_STATUS === "NOT_VERIFIED"){
+    return(
+        <div>
+            <span>You are not verified. Please verify your email or mobile number</span>
+        </div>
+    );
+  } else if (USER_STATUS === "VERIFIED"){
+
+       return( <div>
+            <span>You are verified. Congrats! click here to acces Your Dashboard.</span>
+        </div>)
+
+  } else if (USER_STATUS === "ACCOUNT_DISABLED"){
+
+        return (<div>
+            <span>You account is disabled.</span>
+        </div>)
+
+  }
   
 }
